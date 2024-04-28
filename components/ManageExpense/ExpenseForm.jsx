820d483b8ff719d2ea5b230eaee6a9d700593e53
Harsh <hs5686584@gmail.com>
2024-04-29 00:40:02 +0530

    adding input functionality and created method binding to input handler

--- components/ManageExpense/ExpenseForm.jsx
+++ components/ManageExpense/ExpenseForm.jsx
@@ -1,36 +1,74 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import Input from './Input';
 
 export default function ExpenseForm() {
-  function amountHandler() {
+  const [inputValues, setInputValues] = useState({
+    amount: '',
+    date: '',
+    description: '',
+  });
+  function inputChangedHandler(inputIdentifier, enteredValue) {
+    setInputValues(curInputValues => {
+      return {
+        ...curInputValues,
+        [inputIdentifier]: enteredValue,
+      };
+    });
     console.log('button clicked');
   }
   return (
-    <View>
-      <Input
-        label="Amount"
-        textInputConfig={{
-          keyboardType: 'decimal-pad',
-          onChangeText: amountHandler,
-        }}
-      />
-      <Input
-        label="Date"
-        textInputConfig={{
-          placeholder: 'YYYY-MM-DD',
-          maxLenth: 10,
-          onChangeText: () => {},
-        }}
-      />
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expenses</Text>
+      <View style={styles.inputRow}>
+        <Input
+          style={styles.rowInput}
+          label="Amount"
+          textInputConfig={{
+            keyboardType: 'decimal-pad',
+            onChangeText: inputChangedHandler.bind(this, 'amount'),
+            value: inputValues.amount,
+          }}
+        />
+        <Input
+          style={styles.rowInput}
+          label="Date"
+          textInputConfig={{
+            placeholder: 'YYYY-MM-DD',
+            maxLenth: 10,
+            onChangeText: inputChangedHandler.bind(this, 'date'),
+            value: inputValues.date,
+          }}
+        />
+      </View>
       <Input
         label="Description"
         textInputConfig={{
           multiline: true,
+          onChangeText: inputChangedHandler.bind(this, 'description'),
+          value: inputValues.description,
         }}
       />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  rowInput: {
+    flex: 1,
+  },
+  form: {
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+    marginVertical: 18,
+    textAlign: 'center',
+  },
+});
