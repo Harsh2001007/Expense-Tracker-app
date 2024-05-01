3db5dcd6280ec691bcead40b6ff6518726206258
Harsh <hs5686584@gmail.com>
2024-05-01 23:32:37 +0530

    adding get method for axios

--- utils/http.js
+++ utils/http.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import {BackHandler} from 'react-native';
 
+const BACKEND_URL = 'https://expense-tracker-fb8b2-default-rtdb.firebaseio.com';
 export function storeExpenses(expenseData) {
-  axios.post(
-    'https://expense-tracker-fb8b2-default-rtdb.firebaseio.com/expenses.json',
-    expenseData,
-  );
+  axios.post(BACKEND_URL + '/expenses.json', expenseData);
+}
+
+export function fetchResponse() {
+  axios.get(BACKEND_URL + '/expense.json');
 }
